ac1a278b75358c7dfd2d8d8757b5f7fb37491e58
RobertMisch <robertmisch94@gmail.com>
2020-04-27 15:51:05 -0400

    added plan and some redux templates

---
+++ potluck-frontend/src/store/reducers/authReducer.js
@@ -0,0 +1,15 @@
+// import
+
+
+export const banna = {
+    id:"",
+    username:"",
+    password:"",
+}
+
+export const authReducer = (state = banna, action) => {
+    switch (action.type) {
+        default:
+        return state;
+    }
+}
