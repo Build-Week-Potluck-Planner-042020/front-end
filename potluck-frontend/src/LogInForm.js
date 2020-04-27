50743df0d356db7361887e624cdadf1b0646c51a
unknown <prakashbahadursingh2@gmail.com>
2020-04-27 12:34:11 -0700

    log in form created

---
+++ potluck-frontend/src/LogInForm.js
@@ -0,0 +1,53 @@
+// LogInForm
+
+import React, {useState, UseEffect} from 'react'
+import './App.css';
+import axios from 'axios'
+import * as yup from 'yup'
+import styled from "styled-components";
+
+const url = 'https://reqres.in/api/login'
+
+
+
+
+function LogInForm (props){
+    const{
+        values,
+        onInputChange,
+        onSubmit,
+        disabled,
+        errors,
+    } = props
+
+    return(
+        <form>
+            <h1> Please Log In</h1>
+            <div>
+                <p>{errors.username}</p>
+                <p>{errors.password}</p>
+            </div>
+
+            <label> Username: </label> 
+                <input  value = {values.username}
+                        onChange = {onInputChange}
+                        name = 'username'
+                        type = 'text'/>
+            <label> Password: </label>
+                <input  value = {values.password}
+                        onChange = {onInputChange}
+                        name = 'password'
+                        type = 'password'/>
+
+           <button onClick = {onSubmit} disabled = {disabled}>Log In</button>  
+            
+           
+
+
+        </form>
+
+
+
+    )
+}
+export default LogInForm
