1f305319b78308d8ad39240bf3c1b21070def16e
unknown <prakashbahadursingh2@gmail.com>
2020-04-29 08:56:26 -0700

    unnecessary codes deleted

--- potluck-frontend/src/App.js
+++ potluck-frontend/src/App.js
@@ -15,18 +15,6 @@ const initialFormValues = {
     username : '',
     password : '',
     location: '',
-    // firstName : '',
-    // lastName: '',
-    // dateOfBirth: '',
-    // address: '',
-    // street: '',
-    // city: '',
-    // state: '',
-    // zipCode: '',
-//     role:{
-//       organizer: false,
-//       participant: false,
-//     }
 }
 
 //shape of the validation errors
@@ -34,15 +22,6 @@ const initialFormErrors = {
   username : '',
   password : '',
   location: '',
-  // firstName : '',
-  // lastName: '',
-  // dateOfBirth: '',
-  // address: '',
-  // street: '',
-  // city: '',
-  // state: '',
-  // zipCode: '',
-  // role: '',
 }
 
 //setting validation with error message
@@ -59,44 +38,6 @@ const formErrorCheck = yup.object().shape({
   .string()
   .min(5, 'location must have at least 5 characters in length!')
   .required('location is required'),
-  // firstName: yup
-  //   .string()
-  //   .min(5, 'firstName must have at least 5 characters in length!')
-  //   .required('firstName is required'),
-  // lastName: yup
-  //   .string()
-  //   .min(5, 'lastName must have at least 5 characters in length!')
-  //   .required('lastName is required'),
-  // dateOfBirth: yup
-  //   .string()
-  //   .min(5, 'dateOfBirth must have at least 5 characters in length!')
-  //   .required('dateOfBirth is required'),
-  // gender: yup
-  //   .string()
-  //   .matches(/(male|female)/, 'either male or female')
-  //   .required('gender is required'),
-  // street: yup
-  //   .string()
-  //   .min(5, 'street must have at least 5 characters in length!')
-  //   .required('street is required'),
-  // city: yup
-  //   .string()
-  //   .min(5, 'city must have at least 5 characters in length!')
-  //   .required('city is required'),
-  // state: yup
-  //   .string()
-  //   .min(5, 'Username must have at least 5 characters in length!')
-  //   .required('Username is required'),
-  // zipCode: yup
-  //   .string()
-  //   .min(5, 'zipCode must have at least 5 characters in length!')
-  //   .required('zipCode is required'),
-  // role: yup
-  //   .string()
-  //   .matches(/(organizer|participant)/, 'either organizer or participant')
-  //   .required('role is required'),
-
-
 })
 
 
@@ -160,16 +101,6 @@ function App () {
           username: formValues.username,
           password: formValues.password,
           location: formValues.location,
-          // firstName: formValues.firstName,
-          // lastName: formValues.lastName,
-          // dateOfBirth: formValues.dateOfBirth,
-          // address: formValues.address,
-          // street: formValues.street,
-          // city: formValues.city,
-          // state: formValues.state,
-          // zipCode: formValues.zipCode,
-          // role: formValues.role,
-          // state: formValues.state,
         }
           //this post new user to API
         //  setUsers([...users, newRegister]) 
@@ -202,23 +133,9 @@ function App () {
         });
       };
 
-      const onCheckboxChange = evt => {
-        const {name} = evt.target.name
-        const isChecked = evt.target.checked
-
-        setFormValues({
-          ...formValues,
-          role: {
-            ...formValues.role,
-            [name]: isChecked,
-          }
-        })
-      }
-
     return (
         <RegisterForm values={formValues}
         onInputChange={onInputChange}
-        // onCheckboxChange = {onCheckboxChange}
         onSubmit={onSubmit}
         disabled={formDisabled}
         errors={formErrors} />
---
+++ potluck-frontend/src/useLater.js
@@ -0,0 +1,97 @@
+
+        // firstName : '',
+    // lastName: '',
+    // dateOfBirth: '',
+    // address: '',
+    // street: '',
+    // city: '',
+    // state: '',
+    // zipCode: '',
+//     role:{
+//       organizer: false,
+//       participant: false,
+//     }   
+       
+       
+       
+       // firstName : '',
+  // lastName: '',
+  // dateOfBirth: '',
+  // address: '',
+  // street: '',
+  // city: '',
+  // state: '',
+  // zipCode: '',
+  // role: '',  
+       
+       
+         // firstName: yup
+  //   .string()
+  //   .min(5, 'firstName must have at least 5 characters in length!')
+  //   .required('firstName is required'),
+  // lastName: yup
+  //   .string()
+  //   .min(5, 'lastName must have at least 5 characters in length!')
+  //   .required('lastName is required'),
+  // dateOfBirth: yup
+  //   .string()
+  //   .min(5, 'dateOfBirth must have at least 5 characters in length!')
+  //   .required('dateOfBirth is required'),
+  // gender: yup
+  //   .string()
+  //   .matches(/(male|female)/, 'either male or female')
+  //   .required('gender is required'),
+  // street: yup
+  //   .string()
+  //   .min(5, 'street must have at least 5 characters in length!')
+  //   .required('street is required'),
+  // city: yup
+  //   .string()
+  //   .min(5, 'city must have at least 5 characters in length!')
+  //   .required('city is required'),
+  // state: yup
+  //   .string()
+  //   .min(5, 'Username must have at least 5 characters in length!')
+  //   .required('Username is required'),
+  // zipCode: yup
+  //   .string()
+  //   .min(5, 'zipCode must have at least 5 characters in length!')
+  //   .required('zipCode is required'),
+  // role: yup
+  //   .string()
+  //   .matches(/(organizer|participant)/, 'either organizer or participant')
+  //   .required('role is required'),
+
+       
+       
+       // const newRegister = {
+        //     username: formValues.username,
+        //     password: formValues.password,
+        //     location: formValues.location,
+            // firstName: formValues.firstName,
+            // lastName: formValues.lastName,
+            // dateOfBirth: formValues.dateOfBirth,
+            // address: formValues.address,
+            // street: formValues.street,
+            // city: formValues.city,
+            // state: formValues.state,
+            // zipCode: formValues.zipCode,
+            // role: formValues.role,
+            // state: formValues.state,
+          }
+
+                // const onCheckboxChange = evt => {
+      //   const {name} = evt.target.name
+      //   const isChecked = evt.target.checked
+
+      //   setFormValues({
+      //     ...formValues,
+      //     role: {
+      //       ...formValues.role,
+      //       [name]: isChecked,
+      //     }
+      //   })
+      // }
+
+              // onCheckboxChange = {onCheckboxChange}
+
